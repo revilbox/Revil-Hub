8372bc2e1c3cd916cfe27b975a2093bdaade0f46
H1ki <bee21rire@gmail.com>
2026-05-06 15:31:38 +0700

    Update InitModules.lua

--- Resources/Hub/Modules/InitModules.lua
+++ Resources/Hub/Modules/InitModules.lua
@@ -1,8 +1,7 @@
 return {
     init = function(callback)
         if callback == nil then callback = function() end end;
-        local moduleLinks = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://raw.githubusercontent.com/debug420/Ez-Hub/"..
-        (_G.EzHubDevMode and "dev" or "master").."/Modules/Modules.json"));
+        local moduleLinks = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://raw.githubusercontent.com/revilbox/Revil-Hub/refs/heads/main/Resources/Hub/Modules/Modules.json"));
 
         local moduleIndex = 0;
         local moduleNumber = (function()
@@ -12,16 +11,4 @@ return {
             end
             return count;
         end)()
-        
-        _G["EzHubModules"] = {};
-        for i, v in pairs(moduleLinks) do
-            moduleIndex = moduleIndex + 1;
-            callback(moduleIndex, moduleNumber, i);
-            if _G.EzHubDevMode then v = v:gsub("/master/", "/dev/") end
-            local moduleStringData = game:HttpGet(v);
-            _G["EzHubModules"][i] = moduleStringData;
-        end
-
-        return moduleLinks;
-    end
 }
